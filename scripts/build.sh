f2853c09fea9b357a0e7fc7fa8e984cab1c3d099
John McClane <rmcknight@mba2022.hbs.edu>
2024-10-11 09:56:25 -0400

    Scripts

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Build the application using the build Dockerfile
+docker build -t lsp-box-builder -f dockerfiles/build .
+
+# Run the builder container to create the binary
+if ! docker run --rm -v "$(pwd)":/usr/src/app lsp-box-builder; then
+    echo "Build failed. Exiting."
+    exit 1
+fi
