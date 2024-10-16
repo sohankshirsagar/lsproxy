59b3279837d4a6d6954d6892331c12256bb3ff65
Bjbrostoff <ben.brostoff@pastel.network>
2024-10-15 18:14:59 -0600

    first sample project in python

---
+++ sample_project/python_venv/src/AStarGraph.py
@@ -0,0 +1,32 @@
+class AStarGraph(object):
+	#Define a class board like grid with two barriers
+
+	def __init__(self):
+		self.barriers = []
+		self.barriers.append([(2,4),(2,5),(2,6),(3,6),(4,6),(5,6),(5,5),(5,4),(5,3),(5,2),(4,2),(3,2)])
+
+	def heuristic(self, start, goal):
+		#Use Chebyshev distance heuristic if we can move one square either
+		#adjacent or diagonal
+		D = 1
+		D2 = 1
+		dx = abs(start[0] - goal[0])
+		dy = abs(start[1] - goal[1])
+		return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+
+	def get_vertex_neighbours(self, pos):
+		n = []
+		#Moves allow link a chess king
+		for dx, dy in [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,1),(1,-1),(-1,-1)]:
+			x2 = pos[0] + dx
+			y2 = pos[1] + dy
+			if x2 < 0 or x2 > 7 or y2 < 0 or y2 > 7:
+				continue
+			n.append((x2, y2))
+		return n
+
+	def move_cost(self, a, b):
+		for barrier in self.barriers:
+			if b in barrier:
+				return 100 #Extremely high cost to enter barrier squares
+		return 1 #Normal movement cost
---
+++ sample_project/readme.md
@@ -0,0 +1,7 @@
+### Overview
+This sample project is designed to replicate some of the challenges you might face with a semicomplex project using multiple languages
+
+We take code from:
+https://rosettacode.org/wiki/A*_search_algorithm
+
+This code is spread out over multiple files unrealistically, to demonstrate the ability to find, search, and modify with disparate and mixed files.
