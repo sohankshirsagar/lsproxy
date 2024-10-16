9d16a0c7e8c9cb948d1cc5123537c75038c91382
Rob <rmcknight451@gmail.com>
2024-10-16 11:04:55 -0400

    Move rust down and update scripts

--- scripts/generate_spec.sh
+++ scripts/generate_spec.sh
@@ -3,16 +3,16 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Build the application using the build Dockerfile
-docker build -t lsp-box-builder -f dockerfiles/build .
+docker build -t lsp-box-builder -f dockerfiles/build lsproxy
 
 # Run the builder container to create the binary
-if ! docker run --rm -v "$(pwd)":/usr/src/app lsp-box-builder; then
+if ! docker run --rm -v "$(pwd)/lsproxy":/usr/src/app lsp-box-builder; then
     echo "Build failed. Exiting."
     exit 1
 fi
 
 # Build the runner image
-docker build -t lsp-box-runner -f dockerfiles/run .
+docker build -t lsp-box-runner -f dockerfiles/run lsproxy
 
 # Run the application to generate the OpenAPI spec
 docker run --name temp_lsp_box lsp-box-runner -w
