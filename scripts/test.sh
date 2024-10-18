0831a7e40035f9bbb43fed5c890defed17c8dab8
Rob <rmcknight451@gmail.com>
2024-10-18 14:41:12 -0400

    openapi test

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 ./scripts/build.sh
 
-if ! docker run --rm -v "$(pwd)/lsproxy":/usr/src/app lsproxy-dev cargo llvm-cov; then
+if ! docker run --rm -v "$(pwd)/lsproxy":/usr/src/app -v "$(pwd)":/mnt/lsproxy_root lsproxy-dev cargo llvm-cov; then
     echo "Tests failed. Exiting."
     exit 1
 fi
