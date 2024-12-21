d06a89eed52983f0fe4df6329d40e779d2208a3d
Rob <rmcknight451@gmail.com>
2024-12-21 01:44:47 -0800

    fmt

--- lsproxy/src/lsp/manager/language_tests/mod.rs
+++ lsproxy/src/lsp/manager/language_tests/mod.rs
@@ -1,16 +1,19 @@
 use lsp_types::{GotoDefinitionResponse, Location, Range, Url};
 use tokio::time::{sleep, Duration};
 
-use crate::test_utils::{go_sample_path, c_sample_path, typescript_sample_path, cpp_sample_path, js_sample_path, java_sample_path, python_sample_path, rust_sample_path, TestContext};
+use crate::test_utils::{
+    c_sample_path, cpp_sample_path, go_sample_path, java_sample_path, js_sample_path,
+    python_sample_path, rust_sample_path, typescript_sample_path, TestContext,
+};
 
-use crate::api_types::{FileRange, FilePosition, Position, Symbol, SymbolResponse};
+use crate::api_types::{FilePosition, FileRange, Position, Symbol, SymbolResponse};
 
-mod python_tests;
-mod js_tests;
+mod c_tests;
 mod cpp_tests;
+mod golang_tests;
 mod java_tests;
+mod js_tests;
+mod python_tests;
 mod rust_tests;
 mod tsx_tests;
-mod c_tests;
-mod golang_tests;
 mod typescript_tests;
